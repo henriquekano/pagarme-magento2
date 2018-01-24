851d15c5b5cdcab148db5f5a75c2e23d43f33fbe
henriquekano <henriquekano@hotmail.com>
2018-01-24 19:27:31 -0200

    Base for cc tx

---
+++ code/PagarMe/CreditCard/Model/PagarmeCreditCard.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace PagarMe\CreditCard\Model;
+
+
+
+/**
+ * Pay In Store payment method model
+ */
+class PagarmeCreditCard extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    /**
+     * Payment code
+     *
+     * @var string
+     */
+    protected $_code = 'pagarmecreditcard';
+
+    /**
+     * Availability option
+     *
+     * @var bool
+     */
+    protected $_isOffline = true;
+
+
+  
+
+}
