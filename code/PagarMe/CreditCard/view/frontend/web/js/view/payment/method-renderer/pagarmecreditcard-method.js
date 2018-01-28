2a26aecc9a9d66e1653404ddfbc9c567b6643e32
Henrique Kano <henriquekano@hotmail.com>
2018-01-28 21:39:32 -0200

    card hash creation

--- code/PagarMe/CreditCard/view/frontend/web/js/view/payment/method-renderer/pagarmecreditcard-method.js
+++ code/PagarMe/CreditCard/view/frontend/web/js/view/payment/method-renderer/pagarmecreditcard-method.js
@@ -7,18 +7,26 @@
 define(
     [
       'Magento_Payment/js/view/payment/cc-form',
+      'ko',
       'jquery',
       'pagarme',
     ],
-    function (Component, $, pagarme) {
-      'use strict';
+    function (Component, ko, $, pagarme) {
+      'use strict'
       
-      console.log(window.checkoutConfig.payment.pagarme)
       return Component.extend({
         defaults: {
           template: 'PagarMe_CreditCard/payment/pagarmecreditcard',
-          creditCardHoldername: '',
-          cardHash: ''
+          creditCardHoldername: ko.observable(),
+          cardHash: ko.observable()
+        },
+        pagarmeClient: function() {
+          return pagarme.client.connect({
+            encryption_key: this.encryptionKey()
+          })
+        },
+        encryptionKey: function() {
+          return window.checkoutConfig.payment.pagarme.encryptionKey
         },
         isActive: function() {
           return true
@@ -34,7 +42,33 @@ define(
           return false
         },
         createCardHash: function() {
-          
+          var cardExpirationDate = this.formatExpirationDate(
+            this.creditCardExpMonth(), this.creditCardExpYear()
+          )
+          var cardHashPayload = {
+            card_number: this.creditCardNumber(),
+            card_expiration_date: cardExpirationDate,
+            card_cvv: this.creditCardVerificationNumber(),
+            card_holder_name: this.creditCardHoldername()
+          }
+
+          return this.pagarmeClient()
+            .then(client => client.security.encrypt(cardHashPayload))
+            .then(card_hash => {
+              this.cardHash(card_hash)
+            })
+        },
+        formatExpirationDate: function(monthInteger, yearFourDigits) {
+          var month
+          if(parseInt(monthInteger) < 10) {
+            month = "0" + monthInteger
+          } else {
+            month = monthInteger
+          }
+
+          var year = String(yearFourDigits).substr(2)
+
+          return month + year
         }
       })
     }
