5efe9c1dfc2cdebd59f27ef12ea45cee47fc8861
Henrique Kano <henriquekano@hotmail.com>
2018-01-28 02:57:13 -0200

    added 'holder name' field to cc form

--- code/PagarMe/CreditCard/view/frontend/web/js/view/payment/method-renderer/pagarmecreditcard-method.js
+++ code/PagarMe/CreditCard/view/frontend/web/js/view/payment/method-renderer/pagarmecreditcard-method.js
@@ -6,29 +6,37 @@
 /*global define*/
 define(
     [
-        'Magento_Payment/js/view/payment/cc-form',
-        'jquery',
-        'Magento_Checkout/js/action/place-order',
-        'Magento_Checkout/js/model/full-screen-loader',
-        'Magento_Checkout/js/model/payment/additional-validators',
-        'Magento_Payment/js/model/credit-card-validation/validator'
+      'Magento_Payment/js/view/payment/cc-form',
+      'jquery',
+      'Magento_Checkout/js/action/place-order',
+      'Magento_Checkout/js/model/full-screen-loader',
+      'Magento_Checkout/js/model/payment/additional-validators',
+      'Magento_Payment/js/model/credit-card-validation/validator',
     ],
     function (Component, $) {
-        'use strict';
+      'use strict';
 
-        return Component.extend({
-            defaults: {
-                template: 'PagarMe_CreditCard/payment/pagarmecreditcard'
-            },
-            isActive: function() {
-              return true
-            },
-            getCode: function() {
-              return 'pagarmecreditcard'
-            },
-          isShowLegend: () => {
-            true
-          }
-        });
+      var ey = Component.extend({
+        defaults: {
+          template: 'PagarMe_CreditCard/payment/pagarmecreditcard',
+          creditCardHoldername: '',
+          cardHash: ''
+        },
+        isActive: function() {
+          return true
+        },
+        getCode: function() {
+          return 'pagarmecreditcard'
+        },
+        isShowLegend: function() {
+          return true
+        },
+        //Deprecated
+        hasSsCardType: function() {
+          return false
+        }
+      });
+      console.log(ey)
+      return ey
     }
 );
